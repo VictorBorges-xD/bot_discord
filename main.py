b576017bedf90d7b4dd1ff02f1df5a0e09238dde
VictorBorges_xD <victor-g-b-o@hotmail.com>
2026-01-09 04:31:38 -0300

    atualiza o modelo do genai para "gemma-3-27b-it"

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ api_key = os.getenv("api_key")
 discord_token = os.getenv("discord_token")
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemma-3-27b")
+model = genai.GenerativeModel("gemma-3-27b-it")
 
 permissoes = discord.Intents.all() #garante ao bot todas as permissoes do discord
 bot = commands.Bot('.', intents = permissoes, help_command=None)
